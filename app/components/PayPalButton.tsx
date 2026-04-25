976db280b85d2cbca3d0c70cfd6a46e1b56434fe
Charles <tongz9597@gmail.com>
2026-04-25 16:13:11 +0800

    fix paypal

--- app/components/PayPalButton.tsx
+++ app/components/PayPalButton.tsx
@@ -2,24 +2,31 @@
 
 import { useEffect, useRef } from "react";
 
+declare global {
+  interface Window {
+    paypal?: any;
+  }
+}
+
 export default function PayPalButton() {
   const paypalRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const script = document.createElement("script");
     script.src =
-      "https://www.paypal.com/sdk/js?client-id=ATbmRwUuq9O6z0LlAQpKAExOtYxN5Tf47V3yPsWXo6x2Uc3YrLpfbqO8KmpQiFHQM00tNQFhOMs8x4_u&currency=USD;
+      "https://www.paypal.com/sdk/js?client-id=ATbmRwUuq9O6z0LlAQpKAExOtYxN5Tf47V3yPsWXo6x2Uc3YrLpfbqO8KmpQiFHQM00tNQFhOMs8x4_u&currency=USD";
+    script.async = true;
 
     script.onload = () => {
       if (window.paypal) {
         window.paypal
           .Buttons({
-            createOrder: (data, actions) => {
+            createOrder: (data: any, actions: any) => {
               return actions.order.create({
                 purchase_units: [{ amount: { value: "5.00" } }],
               });
             },
-            onApprove: (data, actions) => {
+            onApprove: (data: any, actions: any) => {
               return actions.order.capture().then(() => {
                 alert("支付成功");
               });
